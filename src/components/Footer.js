3f3bf73c51e440600dffe42478cf69e65fda3bfd
a2036913 <a2036913@hotmail.com>
2021-07-13 17:34:58 +0800

    almost

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className='footer-container'>
+          <section
+          className="footer-subscription">
+            
+          </section> 
+        </div>
+    )
+}
+
+export default Footer
